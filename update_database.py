7251c46c4d350cb95ea10a4b593e6ccf9de494eb
Jennifer Brabec <jenniferbrabec@gmail.com>
2017-02-26 20:42:51 -0800

    update_database.py added to add new readings to mongoDB whenever it is run

---
+++ update_database.py
@@ -0,0 +1,33 @@
+def update_database():
+
+    from read_probes import read_probes
+    from pymongo import MongoClient
+
+    client = MongoClient()
+
+    # mongoDB db name is 'temp_probes'
+    db = client.temp_probes
+
+    # Collect the current readings from probes and store in 'result'
+    result = read_probes()
+
+    # mongoDB collection in 'temp_probes' db is 'readings'.
+    # Add these results to db using insert_one command.
+    item_to_add = db.readings.insert_one(result)
+
+    # If insert_one was successful, mongoDB will have generated an inserted_id
+    # value. Check that this value exists and return True for function if so.
+    # Otherwise, function returns None.
+    if item_to_add.inserted_id:
+        return True
+
+
+if __name__ == "__main__":
+
+    # Attempt to update the database when this file is run directly.
+    # Print successful db insertion message if update_database returns True.
+    if update_database():
+        print "Saved new readings to db."
+    # Otherwise indicate failure to update db.
+    else:
+        print "Failed to save new readings to db."
