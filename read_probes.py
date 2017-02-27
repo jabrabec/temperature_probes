f9f4f9727116da55a5ad9ee8b76587934bf5e313
Jennifer Brabec <jenniferbrabec@gmail.com>
2017-02-26 19:45:37 -0800

    comments added to read_probes.py

--- read_probes.py
+++ read_probes.py
@@ -20,31 +20,51 @@ def read_probes():
     import glob
     import arrow
 
+    # Raspberry pi location for DS18B20 probes. Each probe serial starts with
+    # "28-"
     probe_list = glob.glob('/sys/bus/w1/devices/28-*')
 
+    # Initialize empty probe_id list
     probe_ids = []
 
+    # Extract the probe IDs from path folder name
     for item in probe_list:
         probe_ids.append(item.split('/')[-1])
 
+    # Get the current date & time prior to taking readings
     curr_datetime = arrow.now().format()
 
+    # Initialize empty probe_readings dictionary
     probe_readings = {}
 
+    # Open the 'w1_slave' file in each probe folder
     for probe in probe_ids:
         with open('/sys/bus/w1/devices/' + probe + '/w1_slave') as reading:
+            # We only care about the last item on the last line of each
+            # w1_slave file, so initialize temporary list to hold all file
+            # contents that are read.
             temp_list = []
             for line in reading:
                 temp_list.append(line.strip())
+            # Deg. C reading is the last space-separated element on the last
+            # line of the 'w1_slave' file. Use list slicing to eliminate 't='
+            # from that element, convert to float, and divide by 1000 to add in
+            # a decimal point.
             celsius_reading = float(temp_list[-1].split()[-1][2:]) / 1000
+            # Celsius to Fahrenheit conversion equation
             fahren_reading = celsius_reading * 9.0 / 5.0 + 32.0
+            # Add to probe_readings dictionary:
+            # key = last 4 digits of temperature probe ID (is unique key)
+            # value = sub-dictionary of {C:reading, F:reading} current values
             probe_readings[probe[-4:]] = {
                                     "C": celsius_reading,
                                     "F": fahren_reading
                                     }
 
+    # Initialize empty dictionary
     current_reading = {}
 
+    # Add current probe_readings to dictionary with curr_datetime as key
     current_reading[curr_datetime] = probe_readings
 
     return current_reading
@@ -54,6 +74,7 @@ if __name__ == "__main__":
 
     current_reading = read_probes()
 
+    # Pretty-print if file run interactively
     import json
     print json.dumps(current_reading,
                      sort_keys=True,
